008b5d64b223180b0436fe98c7396522636a396c
dineshi3 <dinesh.iyyandurai@vakilsearch.com>
2023-07-16 21:18:30 +0530

    Log Error

--- src/utils/catchAsync.js
+++ src/utils/catchAsync.js
@@ -2,7 +2,7 @@ const logger = require("../config/logger");
 
 const catchAsync = (fn) => (req, res, next) => {
   Promise.resolve(fn(req, res, next)).catch((err) => {
-    logger.error(err.message);
+    logger.error(err);
     if (err.response) logger.error(JSON.stringify(err.response.data));
     next(err);
   });
